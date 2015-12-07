81144f0f6c0c743db8bb6fa939822a6363008ffb
titusz <tp@py7.de>
2015-12-07 20:56:54 +0100

    add basic tooling tasks

---
+++ tasks.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from ftplib import FTP
+from invoke import ctask as task
+from pprint import pprint
+
+from tools import telnet
+
+LIDAR = 'lidar'
+DOTSTAR = 'dotstar'
+
+
+def init(ctx, taskname):
+    if not hasattr(ctx, 'current'):
+        print('First command must be target machine. (eg. $inv lidar reset')
+        sys.exit()
+    else:
+        fill = (taskname, ctx.current.name, ctx.current.host)
+        print("Running task '%s' against '%s' (%s)" % fill)
+
+
+@task
+def show_config(ctx):
+    """Show current configuration"""
+    pprint(dict(ctx))
+
+
+@task
+def lidar(ctx):
+    """Apply command to WiPy with LIDAR Lite"""
+    ctx['current'] = ctx.lidar
+
+
+@task
+def dotstar(ctx):
+    """Apply Command to WiPy with DotStar"""
+    ctx['current'] = ctx.dotstar
+
+
+@task
+def server(ctx):
+    """Apply command to Raspberry PI server"""
+    ctx['current'] = ctx.server
+
+
+@task
+def reset(ctx):
+    """Reset machine"""
+    init(ctx, 'reset')
+    c = ctx.current
+    tclient = telnet.TelnetClient(c.host, c.user, c.pwd)
+    tclient.open()
+    tclient.hard_reset()
+    tclient.close()
+
+
+@task
+def upload(ctx):
+    """Upload code to machine"""
+    init(ctx, 'upload')
+    c = ctx.current
+    if ctx.current.name in (LIDAR, DOTSTAR):
+        ftp = FTP(c.host, c.user, c.pwd)
+        print('Connected to:', ftp.getwelcome())
+        ftp.cwd('/flash')
+        for f in os.listdir(os.getcwd()):
+            if f.endswith('.py') and f not in ctx.blacklist:
+                print('Uploading:', f)
+                with open(f, 'rb') as upload_file:
+                    ftp.storbinary("STOR " + f, upload_file)
+
+
+@task
+def deploy(ctx):
+    """Deploy code to machine (upload & reset)"""
+    upload(ctx)
+    reset(ctx)
---
+++ tools/telnet.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import time
+import logging
+import telnetlib
+
+log = logging.getLogger(__name__)
+
+
+class TelnetClient:
+
+    def __init__(self, host, user, pwd, ttl=15):
+        self.host = host
+        self.user = user
+        self.pwd = pwd
+        self.ttl = ttl
+        self.tn = None
+
+    def soft_reset(self):
+        self.write('raise SystemExit')
+        log.info('Soft reset executed')
+
+    def hard_reset(self):
+        self.write('import machine')
+        self.write('machine.reset()')
+        log.info('Machine reseted.')
+
+    def open(self):
+        self.tn = telnetlib.Telnet(self.host, 23, self.ttl)
+        if b'Login as: ' in self.tn.read_until(b'Login as: ', self.ttl):
+            log.info('Connection established')
+            self.write(self.user)
+            if b'Password:' in self.tn.read_until(b'Password:', self.ttl):
+                time.sleep(0.2)
+                self.write(self.pwd)
+                sentinel = b'for more information.'
+                if sentinel in self.tn.read_until(sentinel, self.ttl):
+                    log.info('Login success.')
+                    return self.tn
+        raise ConnectionError('Connection failed :(')
+
+    def write(self, s):
+        self.tn.write(bytes(s, 'ascii') + b"\r\n")
+
+    def close(self):
+        if self.tn:
+            self.tn.close()
+            self.tn = None
+        log.info('Connection closed')
