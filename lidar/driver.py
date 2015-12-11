2ff868b1e4c214ce7e32e1757fcc2a6f42136cb4
titusz <tp@py7.de>
2015-12-11 18:47:30 +0100

    add lidar driver module

---
+++ lidar/driver.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import time
+from machine import Pin
+from . import config
+
+
+class Lidar:
+    """PWM based driver for Pulselight LIDAR-Lite v2"""
+
+    def __init__(self, p_mon=config.PIN_MONITOR, p_trig=config.PIN_TRIGGER):
+        self.p_trig = Pin(p_trig, mode=Pin.OUT)
+        self.p_mon = Pin(p_mon, mode=Pin.IN, pull=Pin.PULL_UP)
+
+    def distance(self, reads=4):
+        scans = []
+        for f in range(0, reads):
+            self.p_trig.value(0)
+            while not self.p_mon():
+                pass
+            start = time.ticks_us()
+            while self.p_mon():
+                pass
+            pw = time.ticks_diff(start, time.ticks_us()) - 15
+            cm = pw // 10
+            self.p_trig.value(1)
+            scans.append(cm)
+        return sum(scans) // len(scans)
+
+
+if __name__ == "__main__":
+    l = Lidar()
+    while True:
+        print("\r%d" % l.distance(), end=" ")
