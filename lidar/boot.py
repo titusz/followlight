d4f3c5d82745b68630df236e700f84bd3bd4ea90
titusz <tp@py7.de>
2015-12-11 18:41:43 +0100

    update lidar boot to config import

--- lidar/boot.py
+++ lidar/boot.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import machine
 import network
-import netcfg
+import config
 
 wlan = network.WLAN(mode=network.WLAN.STA)
 
-wlan.connect(netcfg.SSID, auth=(network.WLAN.WPA2, netcfg.PWD), timeout=5000)
+wlan.connect(config.SSID, auth=(network.WLAN.WPA2, config.PWD), timeout=5000)
 while not wlan.isconnected():
     machine.idle()
